26ff27940fac75e63b3c92edf7e10f4d3d394158
23tmo <56054998+23tmo@users.noreply.github.com>
2024-08-27 12:50:16 -0700

    Added filter for post date

--- popup/popup.js
+++ popup/popup.js
@@ -3,29 +3,41 @@ console.log("DOM loaded");
 
 function runFunction(){
     chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' });
-
+    //Elements
     const minViewsElement = document.getElementById('minViewsInput');
     const maxViewsElement = document.getElementById('maxViewsInput');
+    const timeValueElement = document.getElementById('time-value');
+    const timeUnitElement = document.getElementById('time-unit');
+
+    // Apply
     const applyButton = document.getElementById('apply');
  
     applyButton.onclick = () => {
         const prefs = {
             minPref: minViewsElement.value,
-            maxPref: maxViewsElement.value
+            maxPref: maxViewsElement.value,
+            timeValuePref: timeValueElement.value,
+            timeUnitPref: timeUnitElement.value
         }
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {message: 'givenThreshold', prefs});
+            chrome.tabs.sendMessage(tabs[0].id, {message: 'newPrefs', prefs});
         });
     }
 
     // Save user prefs until page reload
-    chrome.storage.session.get(["minPref", "maxPref"], (result) => {
-        const { minPref, maxPref } = result;
+    chrome.storage.session.get(["minPref", "maxPref", "timeValuePref", "timeUnitPref"], (result) => {
+        const { minPref, maxPref, timeValuePref, timeUnitPref } = result;
         if (minPref){
             minViewsElement.value = minPref;
         }
         if (maxPref){
             maxViewsElement.value = maxPref;
+        } 
+        if (timeValuePref){
+            timeValueElement.value = timeValuePref;
+        } 
+        if (timeUnitPref){
+            timeUnitElement.value = timeUnitPref;
         } 
     })
 }
