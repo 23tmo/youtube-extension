238c962aaf8d55c002e935e4e4542d06fe31d477
Tallie Molano <talliemo1220@gmail.com>
2024-08-26 19:25:07 -0700

    Added popup UI and refactored mutation observer

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,31 @@
-console.log("This is a popup")
+document.addEventListener("DOMContentLoaded", runFunction);
+console.log("DOM loaded");
+
+function runFunction(){
+    chrome.storage.session.setAccessLevel({ accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS' });
+
+    const minViewsElement = document.getElementById('minViewsInput');
+    const maxViewsElement = document.getElementById('maxViewsInput');
+    const applyButton = document.getElementById('apply');
+ 
+    applyButton.onclick = () => {
+        const prefs = {
+            minPref: minViewsElement.value,
+            maxPref: maxViewsElement.value
+        }
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {message: 'givenThreshold', prefs});
+        });
+    }
+
+    // Save user prefs until page reload
+    chrome.storage.session.get(["minPref", "maxPref"], (result) => {
+        const { minPref, maxPref } = result;
+        if (minPref){
+            minViewsElement.value = minPref;
+        }
+        if (maxPref){
+            maxViewsElement.value = maxPref;
+        } 
+    })
+}
--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,16 +1,38 @@
-(function() {
+var MIN_VIEWS = null;
+var MAX_VIEWS = null;
+
+// Await 'apply' button click to start filter
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    if (request.message === "givenThreshold"){
+        chrome.storage.session.set(request.prefs);
+        MIN_VIEWS = request.prefs.minPref;
+        MAX_VIEWS = request.prefs.maxPref;
+        filterVideos();
+     }
+})
+
+function filterVideos() {
     'use strict';
-    
-    // Number of valid videos within view range
-    var NUM_VIDEOS = 0;
-    var MIN_VIEWS = 10000000;
 
     // Get all homescreen videos
-    var getVideos = function() {
+    var getVideos = function(addedNodes) {
+        // When addedNodes not null, only processes newly added nodes to DOM
+        if (addedNodes){
+            var videos = [];
+            for (let i = 0; i < addedNodes.length; i++) {
+                if(addedNodes[i].parentElement){
+                    if(addedNodes[i].parentElement.id == 'content'){
+                        videos.push(addedNodes[i].parentElement.parentElement);
+                    }
+                }
+            }
+            return videos;
+        }
+        // Initial and rerendering run:
         return document.querySelectorAll('ytd-rich-item-renderer');
     }
 
-    // Get video's views
+    // Get video views
     var getViews = function(video) {
         var metadata = video.querySelector(
             'div#metadata-line'
@@ -23,7 +45,7 @@
                 // Get the text content of the selected element
                 var viewCountText = viewCountElement.textContent; 
                 if (viewCountText === 'No views'){
-                    return null;
+                    return 0;
                 }
 
                 // Remove ' views' and ' watching' labels, keep 'K' and 'M'
@@ -36,7 +58,7 @@
 
     // Convert text with 'K' or 'M' to number for comparison
     var convertToNumber = function(text){
-        // remove commas, convert to float
+        // Remove commas, convert to float
         var viewCountNumber = parseFloat(text.replace(/,/g, ''));
         if (text.includes('K')) {
             return viewCountNumber * 1000;
@@ -48,34 +70,46 @@
     
     var displayVideo = function(video){
         var views = getViews(video);
-        var section = document.querySelector('div#content.style-scope.ytd-rich-section-renderer');
-        if (!views || views <= MIN_VIEWS) {
-            video.remove();
-        } 
-        // Remove movies section if present
+        var section = document.querySelector('ytd-rich-section-renderer');
+
+        if (!views ||  
+            (MAX_VIEWS && views > MAX_VIEWS) ||
+            (MIN_VIEWS && views < MIN_VIEWS)) {
+            // Changing display style instead of remove to reshow if threshold changes
+            video.style.display = 'none'; 
+        }
+        else{
+            video.style.display = 'block';
+        }
+
+        // Removing extra section breaks
         if (section){
             section.remove();
         }
     }
 
-    // Display videos views in console
-    var handleVideos = function(){
-        var videos = getVideos();
-        videos.forEach(displayVideo);
+    // Process and display newly loaded videos
+    var handleVideos = function(addedNodes){
+        var videos = getVideos(addedNodes);
+        if(videos && videos != []){
+            videos.forEach(displayVideo); 
+        }
     }
 
     // Run mutation observer to check for DOM changes
     var run = function() {
         var targetNode = document.querySelector('div#contents.style-scope.ytd-rich-grid-renderer');
         if (targetNode) {
-            const config = { childList: true, subtree: true };
+            const config = { childList: true, subtree: false, attributes: false};
 
             const callback = (mutationList, observer) => {
                 for (const mutation of mutationList) {
-                    if (mutation.addedNodes.length > 0) {
-                        setTimeout(handleVideos, 1000);
+                    if (mutation.addedNodes.length) {
+                        handleVideos(mutation.addedNodes);
                     }
                 }
+                // Run on all videos (included previously handled) regularly to catch re-rendered ones
+                handleVideos(null);
             };
 
             // MutationObserver to watch for new videos being added to the DOM
@@ -84,9 +118,9 @@
             // Start observing the target node for configured mutations
             observer.observe(targetNode, config);
 
-            // Initial run to load the current videos
-            setTimeout(handleVideos, 1000);
-
+            // Initial run to load current videos
+            handleVideos(null);
+            
         } else {
             // If the target node is not found, check again after a short delay
             console.error('Target node not found, retrying...');
@@ -96,4 +130,4 @@
 
     // Initial run call
     run();
-})();
+};
